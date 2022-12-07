924bbf2c2f37dd62364e6cdfe1449fc3c64e73f4
Aziza Murzayeva <99619017+Aziza526@users.noreply.github.com>
2022-12-07 16:33:18 -0600

    Added Murzayeva.java class

--- Itse2417Main.java
+++ Itse2417Main.java
@@ -1,9 +1,10 @@
+
 /** Menu program for ITSE2417 Programming Final Project
  *  Used with Github practice the GitHub process.
- *  @author Steve Smiley, Tarrant County College
- *  @version 1.0
+ *
+ * @author Steve Smiley, Tarrant County College
+ * @version 1.0
  */
-
 // ***************************************************************
 // DO NOT MODIFY THIS SECTION
 // Any imports needed for your program should be included in 
@@ -16,8 +17,8 @@ import java.util.Arrays;
 // END OF IMPORT SECTION
 // ***************************************************************
 
-public class Itse2417Main
-{
+public class Itse2417Main {
+
     // ***********************************************************
     // static variables that can be referenced using Itse2417Main. references
     // DO NOT CHANGE!
@@ -26,75 +27,68 @@ public class Itse2417Main
     static Scanner myScan = new Scanner(System.in);
 
     /**
-     *  For the menu array, replace FinalProj with the name of your class
-     *  Please make sure that you
-     *      (1) DO NOT DELETE keyword new (Your class has to be instantiated)
-     *      (2) Put your code on the correct line
-     *  See Entry 22 for an example.
+     * For the menu array, replace FinalProj with the name of your class Please
+     * make sure that you (1) DO NOT DELETE keyword new (Your class has to be
+     * instantiated) (2) Put your code on the correct line See Entry 22 for an
+     * example.
      *
-     *  Notes:
-     *      Your class will be named Lastname.java
-     *      Use FinalProj.java as your starting code template. @see FinalProj.java
-     *      Can contain menus.
-     *      If you need a Scanner from keyboard, use Itse2417Main.myScan,
-     *          a static Scanner provided by the main menu program.  Using
-     *          your own, local Scanner will cause problems for everyone.
-     *      Will be implementing the begin() method of the FinalProject interface. @see FinalProject.java
-     *      Do whatever you want inside begin().  Main requirement: your program must end cleanly 
-     *          and return to Itse2417Main to display the menu.
-    */
+     * Notes: Your class will be named Lastname.java Use FinalProj.java as your
+     * starting code template. @see FinalProj.java Can contain menus. If you
+     * need a Scanner from keyboard, use Itse2417Main.myScan, a static Scanner
+     * provided by the main menu program. Using your own, local Scanner will
+     * cause problems for everyone. Will be implementing the begin() method of
+     * the FinalProject interface. @see FinalProject.java Do whatever you want
+     * inside begin(). Main requirement: your program must end cleanly and
+     * return to Itse2417Main to display the menu.
+     */
+    static ArrayList<Object> menu = new ArrayList<Object>(
+            Arrays.asList(
+                    new FinalProj(), // Abdo 
+                    new FinalProj(), // Adams  
+                    new FinalProj(), // Benitez
+                    new FinalProj(), // Butler A
+                    new FinalProj(), // Butler K
+                    new FinalProj(), // Castaneda
+                    new FinalProj(), // Cole
+                    new FinalProj(), // Denton
+                    new FinalProj(), // Doan
+                    new FinalProj(), // Gutt
+                    new FinalProj(), // Lopez
+                    new FinalProj(), // Melendez
+                    new FinalProj(), // Murphy
+                    new Murzayeva(), // Murzayeva
+                    new FinalProj(), // Ramirez
+                    new FinalProj(), // Sylvester
+                    new FinalProj(), // Thurman
+                    new FinalProj(), // Vanslyke                                                
+                    new SmileyMenu() // Smiley
 
-    static ArrayList<Object> menu = new ArrayList<Object>  (
-        Arrays.asList(   
-            new FinalProj(),          // Abdo 
-            new FinalProj(),          // Adams  
-            new FinalProj(),          // Benitez
-            new FinalProj(),          // Butler A
-            new FinalProj(),          // Butler K
-            new FinalProj(),          // Castaneda
-            new FinalProj(),          // Cole
-            new FinalProj(),          // Denton
-            new FinalProj(),          // Doan
-            new FinalProj(),          // Gutt
-            new FinalProj(),          // Lopez
-            new FinalProj(),          // Melendez
-            new FinalProj(),          // Murphy
-            new FinalProj(),          // Murzayeva
-            new FinalProj(),          // Ramirez
-            new FinalProj(),          // Sylvester
-            new FinalProj(),          // Thurman
-            new FinalProj(),          // Vanslyke                                                
-            new SmileyMenu()          // Smiley
-                                                    ));
+            ));
     // ***************************************************************
     // END OF MENU OBJECTS LIST
     // ***************************************************************
     // Constants for the menu choices
     final static int MAXCHOICE = menu.size();
     final static int EXIT = 0;
-    
+
     /**
-     * Starting point for the program.
-     *     Displays the menu list as selected by the user
-     *     Executes the entry in the 
-     *  
+     * Starting point for the program. Displays the menu list as selected by the
+     * user Executes the entry in the
+     *
      * @param args - command line arguments (not required for this program)
      * @return void
-    */
-    public static void main(String[] args)
-    {
+     */
+    public static void main(String[] args) {
         // Verified user entry
         int selection = -1;
 
-        do
-        {
+        do {
             // Display the menu and get the user's choice.
             selection = showMenu();
 
-            if(selection > 0)
-            {
+            if (selection > 0) {
                 FinalProject e = (FinalProject) menu.get(selection - 1);
-                e.begin();    
+                e.begin();
             }
         } while (selection != 0);
 
@@ -102,14 +96,13 @@ public class Itse2417Main
     }
 
     /**
-     * Displays the user menu, a list of students in the class numbered.
-     * User selects a number, which is converted to int and returned to main.
-     * 
-     * @return input    User selection from the menu as an int    
-     * @throws NumberFormatException    Selection out of range or nonnumeric
+     * Displays the user menu, a list of students in the class numbered. User
+     * selects a number, which is converted to int and returned to main.
+     *
+     * @return input User selection from the menu as an int
+     * @throws NumberFormatException Selection out of range or nonnumeric
      */
-    static int showMenu() throws NumberFormatException
-    {
+    static int showMenu() throws NumberFormatException {
         int input = -1;
 
         System.out.println("*******************************************************************");
@@ -141,24 +134,20 @@ public class Itse2417Main
         System.out.println("0.  Exit");
         System.out.println();
         System.out.print("Your selection? ");
-        
-        try
-        {
+
+        try {
             input = myScan.nextInt();
-            
-            if( input < EXIT || input > MAXCHOICE)
-            {
+
+            if (input < EXIT || input > MAXCHOICE) {
                 throw new NumberFormatException();
             }
-        }
-        catch (NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             System.out.println();
             System.out.println("*****************     ERROR     *****************");
             System.out.println("Please enter a number from the menu or 0 to exit.");
-            System.out.println("*****************     ERROR     *****************"); 
+            System.out.println("*****************     ERROR     *****************");
             input = -1;
-            System.out.println();   
+            System.out.println();
         }
 
         return input;
---
+++ Murzayeva.java
@@ -0,0 +1,72 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author murza
+ */
+public class Murzayeva implements FinalProject {
+
+    @Override
+    public void begin() throws NumberFormatException {
+        int input;
+
+        System.out.println("********************************************");
+        System.out.println(" Welcome to the Guessing Game");
+        System.out.println("********************************************");
+        System.out.println();
+        System.out.println("Try to guess the number from 1-10. Ready? ");
+        System.out.println("Enter a positive integer between 1 and 10: ");
+
+        try {
+            input = Itse2417Main.myScan.nextInt();
+
+            if (input >= 1 && input <= 10) {
+                Guess(input);
+            } else {
+                throw new NumberFormatException();
+            }
+        } catch (InterruptedException e) {
+
+        } catch (NumberFormatException e) {
+            System.out.println("NUMBER IS OUT OF RANGE! TRY AGAIN!");
+            begin();
+            try {
+                java.util.concurrent.TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException f) {
+            }
+        }
+    }
+
+    public void Guess(int userGuessNumber) throws InterruptedException {
+        int x = 1 + (int) +(Math.random() * 10);
+
+        System.out.println();
+        System.out.println();
+
+        while (userGuessNumber != x) {
+            if (userGuessNumber > x) {
+                System.out.println("Lower!");
+                userGuessNumber = Itse2417Main.myScan.nextInt();
+            } else if (userGuessNumber < x) {
+                System.out.println("Higher!");
+                userGuessNumber = Itse2417Main.myScan.nextInt();
+            }
+
+        }
+
+        System.out.println("Congratulations! You found the Number!");
+
+        System.out.println();
+        System.out.println();
+        java.util.concurrent.TimeUnit.SECONDS.sleep(2);
+
+    }
+
+    public static void main(String[] args) {
+        Murzayeva mur = new Murzayeva();
+        mur.begin();
+    }
+}
